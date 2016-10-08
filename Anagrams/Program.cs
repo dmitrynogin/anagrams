d35b60fdd3b4dcc6f5cb930d9039290bf5bf4032
dmitrynogin <dmitryn@mail.com>
2016-10-07 18:16:49 -0700

    Implement anagram search.

---
+++ Anagrams/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anagrams = File.ReadAllLines(args[0])
+                .ToLookup(w => w.Signature());
+
+            foreach (var word in args.Skip(1))
+            {
+                WriteLine($"Anagrams for \"{word}\":");
+                foreach (var anagram in anagrams[word.Signature()])
+                    WriteLine(anagram);
+
+                WriteLine();
+            }
+        }
+    }
+
+    static class Anagram
+    {
+        public static string Signature(this string word) =>
+            new string(word.ToLowerInvariant().OrderBy(c => c).ToArray());
+    }
+}
